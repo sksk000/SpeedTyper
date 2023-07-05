ebb9c6e17f95fe3e86ae0107eb750ca48c904bbf
sigure1000 <sigure631@gmail.com>
2023-07-06 00:50:46 +0900

    時間関連処理追加

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,13 +1,17 @@
 //必要な変数設定
 var inputdata = document.getElementById("form");
 var sentence = document.getElementById("sentence");
+var timer = document.getElementById("timer");
+
+//定数宣言
+const asksec = 10;
+const addsec = 4;
 
 //タイピングデータをCSVから取得
 var csvdata = document.getElementById("csv").textContent.trim();
 var typedata = new Array(); 
 var splitdata = csvdata.split(",");
 
-console.log(typeof(splitdata[1]));
 //CSVデータを配列に格納
 for(var i = 0; i < csvdata.length; ++i)
 {
@@ -17,15 +21,34 @@ for(var i = 0; i < csvdata.length; ++i)
     }
 }
 
+//最初に表示する文字を設定
+sentence.textContent = typedata[getRandomNum(typedata.length)];
+var count = asksec;
 
-sentence.textContent = typedata[0];
+//設定した秒数経過するたびに呼ばれる設定
+var intervalfunc = setInterval(function()
+{
+    timer.textContent = "Time Left:" + count.toString();
 
+    if(count < 0)
+    {
+        count = 0;
+        clearInterval(intervalfunc);
+    }
+
+    count--;
+},1000)
+
+//入力されたときのイベント設定
 inputdata.addEventListener("input",function()
 {
+    //表示された文字と入力された文字が完全一致しているか確認
     if(sentence.textContent === inputdata.value)
     {
+        //入力されている文字を消して、次の表示する文字をランダムに設定する
         inputdata.value = "";
         sentence.textContent = typedata[getRandomNum(typedata.length)];
+        count += addsec;
     }
 });
 
